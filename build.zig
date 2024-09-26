d5b6b2be5cd5432fc707f21348d04768a03c908e
abvee <icaliberdev@gmail.com>
2024-09-26 14:48:57 +0530

    Hill Climbing build.zig

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
 	const optimize = .ReleaseSafe;
+
 	{
 		const random_graph = b.addExecutable(.{
 			.name = "random-graph",
@@ -20,5 +21,15 @@ pub fn build(b: *std.Build) void {
 			.optimize = optimize,
 		});
 		b.installArtifact(water_jug);
+	}
+
+	{
+		const hill_climbing = b.addExecutable(.{
+			.name = "hill-climbing",
+			.root_source_file = b.path("hill-climbing/hill-climbing.zig"),
+			.target = b.host,
+			.optimize = optimize,
+		});
+		b.installArtifact(hill_climbing);
 	}
 }
