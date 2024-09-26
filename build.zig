24a41ce5d1353c6db11e62b95353af6a48584ea3
abvee <icaliberdev@gmail.com>
2024-09-26 09:31:22 +0530

    Basic Build script

---
+++ build.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+	const optimize = .ReleaseSafe;
+	{
+		const random_graph = b.addExecutable(.{
+			.name = "random-graph",
+			.root_source_file = b.path("random-graph.zig"),
+			.target = b.host,
+			.optimize = optimize,
+		});
+		b.installArtifact(random_graph);
+	}
+
+	{
+		const water_jug = b.addExecutable(.{
+			.name = "water-jug",
+			.root_source_file = b.path("water-jug/water-jug-bfs.zig"),
+			.target = b.host,
+			.optimize = optimize,
+		});
+		b.installArtifact(water_jug);
+	}
+}
