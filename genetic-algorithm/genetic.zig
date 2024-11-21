ae8a455b15a6b96e909e5d16a38ceb251164dfd5
abvee <icaliberdev@gmail.com>
2024-11-21 14:12:07 +0530

    functions for getting fitness, probabilities and expected counts

--- genetic-algorithm/genetic.zig
+++ genetic-algorithm/genetic.zig
@@ -11,30 +11,64 @@ var population: [LEN]u5 = [_]u5{
 	0b10011,
 }; // hardcoded initial population
 
-pub fn main() void {
-	// Initialise initial values
-	// fitness values
-	var fit: [LEN]u32 = [_]u32{0} ** LEN;
-	var fit_sum: u32 = 0;
-	for (&fit, population) |*f, p| {
-		f.* = fitness(p);
-		fit_sum += f.*;
+// calculate fitness values for population and return them
+inline fn fitness_calc(fit_sum: *u32) [LEN]u32 {
+	var ret: [LEN]u32 = [_]u32{0} ** LEN;
+	fit_sum.* = 0;
+	for (&ret, population) |*r, p| {
+		r.* = fitness(p);
+		fit_sum.* += r.*;
 	}
-	const fit_average: f32 = @as(f32, @floatFromInt(fit_sum)) / LEN;
-	std.debug.print("{d:.3}\n", .{fit_average});
+	return ret;
+}
+test "fitness_calc" {
+	var fit_sum: u32 = 0;
+	const fit: [LEN]u32 = fitness_calc(&fit_sum);
+	std.debug.print("{any}\n", .{fit});
+	std.debug.print("{any}\n", .{fit_sum});
+}
 
-	// probabilities
+// probabilities for values in population and return them
+inline fn probabilities(fit: [LEN]u32, fit_sum: u32) [LEN]f32 {
 	var probability: [LEN]f32 = [_]f32{0} ** LEN;
 	for (fit, &probability) |f, *p| {
 		p.* = @as(f32, @floatFromInt(f)) / @as(f32, @floatFromInt(fit_sum));
-		std.debug.print("{d:.3}\n", .{p.*});
 	}
+	return probability;
+}
 
-	// expected count
+// expected count for values in population and return them
+inline fn expected_count(fit: [LEN]u32, fit_average: f32) [LEN]f32 {
 	var expected: [LEN]f32 = [_]f32{0} ** LEN;
 	for (fit, &expected) |f, *e| {
 		e.* = @as(f32, @floatFromInt(f)) / fit_average;
 	}
+	return expected;
+}
+
+pub fn main() void {
+	// Initialise initial values
+	// fitness values
+	var fit_sum: u32 = 0;
+	var fit: [LEN]u32 = fitness_calc(&fit_sum); // fitness
+	var fit_average: f32 = @as(f32, @floatFromInt(fit_sum)) / LEN;
+
+	// probabilities
+	var probability: [LEN]f32 = [_]f32{0} ** LEN;
+	probability = probabilities(fit, fit_sum);
+
+	// expected
+	const expected: [LEN]f32 = expected_count(fit, fit_average);
+
+	std.debug.print("{any}\n{any}\n{any}\n", .{
+		fit,
+		probability,
+		expected,
+	});
+
+	population = mating();
+	fit = fitness_calc(&fit_sum);
+	fit_average = @as(f32, @floatFromInt(fit_sum)) / LEN;
 }
 
 // return popluation after changing it
@@ -42,26 +76,30 @@ fn mating() [LEN]u5 {
 	var ret: [LEN]u5 = population; // return population
 
 	// hardcoded cross over - one is 4, the other is 2
-	_ = enum {FIRST = 4, SECOND = 2};
-	const first_zero_mask = 0b0 << .FIRST;
-	const second_zero_mask = 0b0 << .SECOND;
-
-	const first_clear_mask = 0b01111;
-	const second_clear_mask = 0b00011;
+	var first_mask: u5 = 0b00001;
+	var second_mask: u5 = 0b00011;
 
 	// first section
-	for (&ret[0..LEN / 2]) |*r| {
-		const mutation = ~(r.*|first_clear_mask)|first_clear_mask;
-		r.* |= first_clear_mask;
+	for (ret[0..LEN / 2]) |*r| {
+		const mutation = (~(r.* & first_mask))&first_mask;
+		r.* &= ~first_mask;
 		r.* += mutation;
 	}
 
 	// second section
-	for (&ret[(LEN / 2) + 1..LEN]) |*r| {
-		const mutation = ~(r.*|second_clear_mask)|second_clear_mask;
-		r.* |= second_clear_mask;
+	for (ret[(LEN / 2) + 1..LEN]) |*r| {
+		const mutation = (~(r.* & second_mask))&second_mask;
+		r.* &= ~second_mask;
 		r.* += mutation;
 	}
-
+	first_mask += 1;
+	second_mask += 1;
 	return ret;
+}
+
+test "mating" {
+	var p: [LEN]u5 = population;
+	p = mating();
+	std.debug.print("{any}\n", .{population});
+	std.debug.print("{any}\n", .{p});
 }
