555599f821b8425d74a7bbc560a24d1ce0dc087c
abvee <icaliberdev@gmail.com>
2024-11-28 14:25:16 +0530

    output statements added Binary representation of a number

--- genetic-algorithm/genetic.zig
+++ genetic-algorithm/genetic.zig
@@ -61,19 +61,47 @@ pub fn main() void {
 
 	// expected
 	const expected: [LEN]f32 = expected_count(fit, fit_average);
-	_ = expected;
+
+	stdout.print("Initial Population\tX Value\t\tFitness (X ^ 2)\tProbability\t%Probability\tExpected Count\n", .{})
+		catch {};
+	for (0..LEN) |i| {
+		stdout.print("{s:0<6}\t\t\t{d}\t\t{d}\t\t{d:.3}\t\t{d:.3}\t\t{d}\n", .{
+			binary_representation(population[i]),
+			population[i],
+			fit[i],
+			probability[i],
+			probability[i] * 100,
+			expected[i],
+		}) catch {};
+	}
+	stdout.print("Fitness Sum: {}\tFitness Average {d:.3}\n", .{
+		fit_sum,
+		fit_average,
+	}) catch {};
 
 	// run for specified number of epochs
-	for (0..EPOCHS) |_| {
+	for (0..EPOCHS) |e| {
+		stdout.print("Epoch: {}\n", .{e}) catch {};
 		// perform mating
+		stdout.print("Before mating: {any}\n", .{
+			population,
+		}) catch {};
 		population = mating();
+		stdout.print("After Mating: {any}\n", .{
+			population,
+		}) catch {};
 
 		// perform mutation
 		population = mutation();
+		stdout.print("After Mutation: {any}\n", .{
+			population,
+		}) catch {};
 
 		// recalculate fitness values
 		fit = fitness_calc(&fit_sum);
 		fit_average = @as(f32, @floatFromInt(fit_sum)) / LEN;
+		stdout.print("Fitness: {any}\nFitness Average: {d:.3}\n", .{fit, fit_average}) catch {};
+		stdout.print("\n", .{}) catch {};
 	}
 }
 
@@ -114,6 +142,7 @@ fn mutation() [LEN]u5 {
 	var ret: [LEN]u5 = population; // return population
 	for (&ret) |*r| {
 		const seed = rand.int(u5); // generate random mutation value
+		stdout.print("Mutation: {s}\n", .{binary_representation(seed)}) catch {};
 		r.* ^= seed;
 	}
 	return ret;
@@ -123,4 +152,47 @@ test "mutation" {
 	const p = mutation();
 	std.debug.print("{any}\n", .{population});
 	std.debug.print("{any}\n", .{p});
+}
+
+fn reverse(s: [5]u8) [5]u8 {
+	var i: u8 = 0;
+	var ret: [5]u8 = s;
+
+	var tmp: u8 = 0;
+	while (i < 5/2) : (i += 1) {
+		tmp = ret[5 - i - 1];
+		ret[5 - i - 1] = ret[i];
+		ret[i] = tmp;
+	}
+	return ret;
+}
+test "reverse" {
+	std.debug.print("REVERSE\n", .{});
+	const c: [5]u8 = [_]u8{
+		'0',
+		'0',
+		'0',
+		'0',
+		'1',
+	};
+	std.debug.print("{s}\n", .{reverse(c)});
+}
+
+// take u5, return it as a string
+inline fn binary_representation(x: u5) [5]u8 {
+	var y: u5 = x;
+	var ret: [5]u8 = [1]u8{'0'} ** 5;
+
+	var i: u8 = 0;
+	while (y > 0) : (i += 1) {
+		ret[i] = '0' + @as(u8, @intCast(y % 2));
+		y /= 2;
+	}
+	ret = reverse(ret);
+	return ret;
+}
+test "binary representation" {
+	std.debug.print("BINARY REPRESENTATION\n", .{});
+	std.debug.print("{s:0>5}\n", .{binary_representation(12)});
+	std.debug.print("{s:0>5}\n", .{binary_representation(25)});
 }
